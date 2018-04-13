f67422b890799fd296261a33fd44b1972fe96589
achoo486 <saliraza486@gmail.com>
2018-04-14 03:22:38 +0500

    Done with the model
    Check whether Caffe default bias initialization is zero or not. I have taken it to be same as weight initialization given.
    See batch_normalization function. It is different from the one used in tensorflow code, also its 'training' argument.
    See whether in tensorFlow default deconvolution weight initializer is Xavier or not. Also I have used different function as compared to the tensorFlow Code.
    All these things are also commented in the code.

--- PosAlys/PosAlys.py
+++ PosAlys/PosAlys.py
@@ -1,15 +1,24 @@
 import tensorflow as tf
 import numpy as np
 
-def create_new_conv_layer( input_data, num_input_channels, num_filters, filter_shape, stride, name, activation, initializer ):
-    conv_filter_size = [ filter_shape[ 0 ], filter_shape[ 1 ], num_input_channels, num_filters ]
-    if initializer == 'zeros':
-        init = tf.zeros_initializer()
-    elif initializer == 'xavier':
-        init = tf.glorot_normal_initializer()
+is_training = True
 
-    weights = tf.Variable( init( conv_filter_size ), name = name + '_W' )
-    bias = tf.Variable(tf.truncated_normal([num_filters]), name=name + '_b')
+def create_new_conv_layer( input_data, num_input_channels, num_filters, filter_shape,
+                           stride, name, activation, weight_initializer, bias_initializer ):
+    conv_filter_size = [ filter_shape[ 0 ], filter_shape[ 1 ], num_input_channels, num_filters ]
+
+    if weight_initializer == 'zeros':
+        weight_init = tf.zeros_initializer()
+    elif weight_initializer == 'xavier':
+        weight_init = tf.contrib.layers.xavier_initializer()
+
+    if bias_initializer == 'zeros':
+        bias_init = tf.zeros_initializer()
+    elif bias_initializer == 'xavier':
+        bias_init = tf.contrib.layers.xavier_initializer()
+
+    weights = tf.Variable( weight_init( conv_filter_size ), name = name + '_W' )
+    bias = tf.Variable(bias_init([num_filters]), name=name + '_b')
 
     out_layer = tf.nn.conv2d( input_data, weights, [ 1, stride[ 0 ], stride[ 1 ], 1 ], padding = 'SAME', name = name )
     out_layer += bias
@@ -25,7 +34,8 @@ def create_new_max_pool_layer( input_data, pool_shape, stride, name ):
                                 padding = 'SAME', name = name )
     return out_layer
 
-def create_block(block_name, input, filters, num_input_channels, new_stage, initializer):
+def create_block(block_name, input, filters, num_input_channels, new_stage,
+                 weight_initializer, bias_initializer):
     if new_stage == True:
         first_stride = [ 2, 2 ]
     else:
@@ -33,20 +43,23 @@ def create_block(block_name, input, filters, num_input_channels, new_stage, init
     with tf.name_scope(block_name + '2a'):
         block2a = create_new_conv_layer(input_data=input, num_input_channels=num_input_channels,
                                         num_filters=filters[ 0 ], filter_shape=[1, 1], stride=first_stride,
-                                        name = block_name + '2a_conv', activation=True, initializer = initializer)
+                                        name = block_name + '2a_conv', activation = True,
+                                        weight_initializer = weight_initializer, bias_initializer = bias_initializer)
     with tf.name_scope(block_name + '2b'):
         block2b = create_new_conv_layer(input_data=block2a, num_input_channels=filters[ 0 ],
                                         num_filters=filters[ 1 ], filter_shape=[3, 3], stride=[1, 1],
-                                        name= block_name + '2b_conv', activation=True, initializer = initializer)
+                                        name= block_name + '2b_conv', activation=True,
+                                        weight_initializer = weight_initializer, bias_initializer = bias_initializer )
     with tf.name_scope(block_name + '2c'):
         block2c = create_new_conv_layer(input_data=block2b, num_input_channels=filters[ 1 ],
                                         num_filters=filters[ 2 ], filter_shape=[1, 1], stride=[1, 1],
-                                        name= block_name + '2c_conv', activation=False,  initializer = initializer)
+                                        name= block_name + '2c_conv', activation=False,
+                                        weight_initializer = weight_initializer, bias_initializer = bias_initializer )
     with tf.name_scope('shortcut_' + block_name + '2c'):
         shortcut_block2c = create_new_conv_layer(input_data=input, num_input_channels=num_input_channels,
                                                  num_filters=filters[ 2 ], filter_shape=[1, 1], stride=first_stride,
                                                  name='shortcut_' + block_name + '2c_conv', activation=False,
-                                                 initializer=initializer)
+                                                 weight_initializer = weight_initializer, bias_initializer = bias_initializer)
     with tf.name_scope('shortcut_add_' + block_name + '2c'):
         shortcut_add_block2c = shortcut_block2c + block2c
         shortcut_add_block2c = tf.nn.relu(shortcut_add_block2c)
@@ -61,7 +74,8 @@ with tf.name_scope( 'res1' ):
     with tf.name_scope( 'res1a_conv' ):
         res1a_conv = create_new_conv_layer( input_data = input, num_input_channels = input_channels,
                                             num_filters = 64, filter_shape = [ 7, 7 ], stride = [ 2, 2 ],
-                                            name = 'res1a_conv1', activation = True, initializer = 'zeros')
+                                            name = 'res1a_conv1', activation = True,
+                                            weight_initializer = 'zeros', bias_initializer = 'zeros')
     with tf.name_scope( 'res1a_max_pool' ):
         res1a_max_pool = create_new_max_pool_layer( input_data = res1a_conv, pool_shape = [ 3, 3 ],
                                                     stride = [ 2, 2 ], name = 'res1a_max_pool1' )
@@ -70,75 +84,123 @@ with tf.name_scope( 'res2' ):
     filters_res2 = [ 64, 64, 256 ]
     res2a = create_block(block_name = 'res2a', input = res1a_max_pool,
                         filters = filters_res2, num_input_channels = 64,
-                        new_stage=False, initializer = 'zeros')
+                        new_stage=False, weight_initializer='zeros', bias_initializer='zeros')
     res2b = create_block(block_name='res2b', input=res2a,
                         filters=filters_res2, num_input_channels=filters_res2[ 2 ],
-                        new_stage=False, initializer = 'zeros')
+                        new_stage=False, weight_initializer='zeros', bias_initializer='zeros')
     res2c = create_block(block_name='res2c', input=res2b,
                         filters=filters_res2, num_input_channels=filters_res2[ 2 ],
-                        new_stage=False, initializer = 'zeros')
+                        new_stage=False, weight_initializer='zeros', bias_initializer='zeros')
 
 with tf.name_scope( 'res3' ):
     filters_res3 = [ 128, 128, 512 ]
     res3a = create_block(block_name = 'res3a', input = res2c,
                         filters = filters_res3, num_input_channels = filters_res2[ 2 ],
-                        new_stage=True, initializer = 'zeros')
+                        new_stage=True, weight_initializer='zeros', bias_initializer='zeros')
     res3b = create_block(block_name='res3b', input=res3a,
                         filters=filters_res3, num_input_channels=filters_res3[ 2 ],
-                        new_stage=False, initializer = 'zeros')
+                        new_stage=False, weight_initializer='zeros', bias_initializer='zeros')
     res3c = create_block(block_name='res3c', input=res3b,
                         filters=filters_res3, num_input_channels=filters_res3[ 2 ],
-                        new_stage=False, initializer = 'zeros')
+                        new_stage=False, weight_initializer='zeros', bias_initializer='zeros')
     res3d = create_block(block_name='res3d', input=res3c,
                         filters=filters_res3, num_input_channels=filters_res3[2],
-                        new_stage=False, initializer = 'zeros')
+                        new_stage=False, weight_initializer='zeros', bias_initializer='zeros')
 
 with tf.name_scope( 'res4' ):
     filters_res4 = [ 256, 256, 1024 ]
     res4a = create_block(block_name = 'res4a', input = res3d,
                         filters = filters_res4, num_input_channels = filters_res3[ 2 ],
-                        new_stage=True, initializer = 'zeros')
+                        new_stage=True, weight_initializer='zeros', bias_initializer='zeros')
     res4b = create_block(block_name='res4b', input=res4a,
                         filters=filters_res4, num_input_channels=filters_res4[ 2 ],
-                        new_stage=False, initializer = 'zeros')
+                        new_stage=False, weight_initializer='zeros', bias_initializer='zeros')
     res4c = create_block(block_name='res4c', input=res4b,
                         filters=filters_res4, num_input_channels=filters_res4[ 2 ],
-                        new_stage=False, initializer = 'zeros')
+                        new_stage=False, weight_initializer='zeros', bias_initializer='zeros')
     res4d = create_block(block_name='res4d', input=res4c,
                         filters=filters_res4, num_input_channels=filters_res4[2],
-                        new_stage=False, initializer = 'zeros')
+                        new_stage=False, weight_initializer='zeros', bias_initializer='zeros')
     res4e = create_block(block_name='res4e', input=res4d,
                         filters=filters_res4, num_input_channels=filters_res4[ 2 ],
-                        new_stage=False, initializer = 'zeros')
+                        new_stage=False, weight_initializer='zeros', bias_initializer='zeros')
     res4f = create_block(block_name='res4f', input=res4e,
                         filters=filters_res4, num_input_channels=filters_res4[2],
-                        new_stage=False, initializer = 'zeros')
+                        new_stage=False, weight_initializer='zeros', bias_initializer='zeros')
 
 with tf.name_scope( 'res5' ):
+    #check deault bias_initializer in caffe
     filters_res5 = [ 512, 512, 1024 ]
     res5a = create_block(block_name = 'res5a', input = res4f,
                         filters = filters_res5, num_input_channels = filters_res4[ 2 ],
-                        new_stage=False, initializer = 'xavier')
-    with tf.name_scope('res5b'):
-        with tf.name_scope('res5b_conv1'):
-            res5b = create_new_conv_layer( input_data = res5a, num_input_channels = filters_res5[ 2 ],
-                                           num_filters = 256, filter_shape = [ 1, 1 ], stride = [ 1, 1 ],
-                                           name = 'res5b_conv1', activation = True, initializer = 'xavier')
-        with tf.name_scope('res5b_conv2'):
-            res5b = create_new_conv_layer(input_data=res5b, num_input_channels=256,
-                                          num_filters=128, filter_shape=[3, 3], stride=[1, 1],
-                                          name='res5b_conv2', activation=True, initializer = 'xavier')
-        with tf.name_scope('res5b_conv2'):
-            res5b = create_new_conv_layer(input_data=res5b, num_input_channels=128,
-                                          num_filters=256, filter_shape=[1, 1], stride=[1, 1],
-                                          name='res5b_conv3', activation=True, initializer = 'xavier')
+                        new_stage=False, weight_initializer='xavier', bias_initializer='xavier')
+    with tf.name_scope('res5b2a'):
+        res5b = create_new_conv_layer( input_data = res5a, num_input_channels = filters_res5[ 2 ],
+                                       num_filters = 256, filter_shape = [ 1, 1 ], stride = [ 1, 1 ],
+                                       name = 'res5b_conv1', activation = True,
+                                       weight_initializer='xavier', bias_initializer='xavier')
+    with tf.name_scope('res5b2b'):
+        res5b = create_new_conv_layer(input_data=res5b, num_input_channels=256,
+                                      num_filters=128, filter_shape=[3, 3], stride=[1, 1],
+                                      name='res5b_conv2', activation=True,
+                                      weight_initializer='xavier', bias_initializer='xavier')
+    with tf.name_scope('res5b2c'):
+        res5b = create_new_conv_layer(input_data=res5b, num_input_channels=128,
+                                      num_filters=256, filter_shape=[1, 1], stride=[1, 1],
+                                      name='res5b_conv3', activation=True,
+                                      weight_initializer='xavier', bias_initializer='xavier')
+
+with tf.name_scope('upper_layer'):
+    upper_layer = tf.contrib.layers.conv2d_transpose(res5b, kernel_size=4, num_outputs=128,
+                                                     activation_fn=None, stride=2,
+                                                     padding='same', biases_initializer=False)
+
+    #in code it was is_training
+    upper_layer_batch_normalization = tf.layers.batch_normalization(upper_layer, scale = True, training = is_training,
+                                                           name='batch_normalization')
+
+    upper_layer_batch_normalization = tf.nn.relu(upper_layer_batch_normalization)
+
+with tf.name_scope('order_1_joint_positions_layer'):
+    #default Xavier initializtion
+    #argument names different from code
+    order_1_joints = tf.contrib.layers.conv2d_transpose( res5b, kernel_size = 4, num_outputs = 63,
+                                                         activation_fn = None, stride = 2,
+                                                         padding = 'same', biases_initializer = False)
+
+    delta_x, delta_y, delta_z = tf.split(order_1_joints, num_or_size_splits = 3, axis = 3)
+
+with tf.name_scope('Bone_Lengths'):
+    square = tf.multiply(order_1_joints, order_1_joints, name='square')
+
+    delta_x_square, delta_y_square, delta_z_square = tf.split(square, num_or_size_splits = 3, axis = 3)
+
+    bone_length_square = tf.add(tf.add(delta_x_square, delta_y_square), delta_z_square)
+
+    bone_length = tf.sqrt(bone_length_square)
 
 
-a = res4f + 1
+concatenated = tf.concat([ upper_layer_batch_normalization, delta_x, delta_y, delta_z, bone_length],
+                            axis=3, name='concatenated')
+
+with tf.name_scope('conv_128'):
+    conv_128 = create_new_conv_layer( input_data = concatenated, num_input_channels = 128 + 63 + 21,
+                                      num_filters = 128, filter_shape = [ 3, 3 ], stride = [ 1, 1 ],
+                                      name = 'conv_128', activation = True, weight_initializer = 'xavier',
+                                      bias_initializer = 'zeros')
+with tf.name_scope('conv_84'):
+    conv_84 = create_new_conv_layer( input_data = conv_128, num_input_channels = 128,
+                                      num_filters = 84, filter_shape = [ 1, 1 ], stride = [ 1, 1 ],
+                                      name = 'conv_84', activation = False, weight_initializer = 'xavier',
+                                      bias_initializer = 'zeros')
+
+heatmap, x_location_map, y_location_map, z_location_map = tf.split(conv_84,
+                                                                   num_or_size_splits = 4, axis = 3)
+
 
 init_op = tf.global_variables_initializer()
 with tf.Session() as sess:
     writer = tf.summary.FileWriter('logs', sess.graph)
     sess.run( init_op )
-    sess.run( a, feed_dict = {input : np.ones( [ 10, 256, 256, 3 ] )} )
+    sess.run( [heatmap, x_location_map, y_location_map, z_location_map], feed_dict = {input : np.ones( [ 10, 256, 256, 3 ] )} )
     writer.close()
